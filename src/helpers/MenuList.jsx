47a20ba5d15d9157e7174b73b2149c1a7c87bb2c
KingGift17 <obazunu17@gmail.com>
2023-07-20 20:29:24 +0100

    Added menuitems to menulist along with images

---
+++ src/helpers/MenuList.jsx
@@ -0,0 +1,39 @@
+import KoopaSpecial from "../assets/koopaSpecial.png";
+import CheeseBurger from "../assets/cheeseBurger.jpg";
+import ChickenBurger from "../assets/chickenBurger.png";
+import ChickenWrap from "../assets/chickenWrap.png";
+import TroopaBurger from "../assets/troopaBurger.png";
+import GrassWrap from "../assets/grassWrap.png";
+
+export const MenuList = [
+  {
+    name: "Koopa Special Burger Meal",
+    image: KoopaSpecial,
+    price: 6.5,
+  },
+  {
+    name: "Cheese Burger Meal",
+    image: CheeseBurger,
+    price: 4.25,
+  },
+  {
+    name: "Chicken Burger Meal",
+    image: ChickenBurger,
+    price: 5.99,
+  },
+  {
+    name: "Chicken Wrap",
+    image: ChickenWrap,
+    price: 4.3,
+  },
+  {
+    name: "Troopa without the Koopa Burger",
+    image: TroopaBurger,
+    price: 6.5,
+  },
+  {
+    name: "Grass Wrap",
+    image: GrassWrap,
+    price: 6.5,
+  },
+];
