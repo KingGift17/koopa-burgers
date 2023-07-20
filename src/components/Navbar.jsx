044041057e784ffb90453ecb0e37740fc5abf76e
KingGift17 <obazunu17@gmail.com>
2023-07-20 13:57:41 +0100

    Fixed styling, added toggle button when on phone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/hamburgerlogo.png";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import MenuIcon from "@mui/icons-material/Menu";
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className="navbar">
-      <div className="leftSide">
-        <img src={Logo} />{" "}
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <button>
+        <button onClick={toggleNavbar}>
           <MenuIcon />
         </button>
       </div>
