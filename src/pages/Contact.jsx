12d9fde62a9951e99e6a1ee174561cfc999af1d6
KingGift17 <obazunu17@gmail.com>
2023-07-20 22:47:53 +0100

    Contact page added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Troopa from "../assets/Paratroopart.png";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${Troopa})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Contact Us</h1>
+        <form id="contact-form" method="POST">
+          <label htmlFor="name">Full Name</label>
+          <input name="name" placeholder="Enter full name..." type="text" />
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            placeholder="Enter email address..."
+            type="email"
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            rows="6"
+            placeholder="Enter message..."
+            name="message"
+            required
+          ></textarea>
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
